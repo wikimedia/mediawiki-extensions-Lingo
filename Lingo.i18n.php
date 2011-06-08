7a5a346df9a0bfaa7a0e72e468ef6056b8b92779
Raimond Spekking <raymond@users.mediawiki.org>
2011-06-08 21:24:03 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-06-08 20:51:00 UTC)

--- Lingo.i18n.php
+++ Lingo.i18n.php
@@ -16,8 +16,44 @@ $messages['en'] = array(
 
 );
 
+/** Message documentation (Message documentation) */
 $messages['qqq'] = array(
 	'lingo-desc' => '{{desc}}',
-	'lingo-terminologypagename' => 'Name of the page where the terms and definitions of the glossary are stored'
+	'lingo-terminologypagename' => 'Name of the page where the terms and definitions of the glossary are stored',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'lingo-desc' => 'Ermöglicht Informationstexte die eingeblendet werden, sobald man mit der Maus über einen auf der Seite [[Terminology]] hinterlegten Begriff führt',
+);
+
+/** French (Français)
+ * @author Sherbrooke
+ */
+$messages['fr'] = array(
+	'lingo-desc' => 'Dans les pages, affiche une info-bulle si le mot est défini dans la page de [[Terminology|Terminologie]].',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'lingo-desc' => 'Овозможува описи при лебдење над алатките на страниците (од зборови со утврдени значења на страницата [[Terminology|Терминологија]])',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'lingo-desc' => 'Permite que a passagem do ponteiro do rato sobre palavras nas páginas, resulte no aparecimento de uma legenda. As palavras são definidas na página [[Terminology]].',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'lingo-desc' => 'Показывает всплывающую подсказку при наведении на слово, присутствующее на странице [[Terminology]]',
 );
 
