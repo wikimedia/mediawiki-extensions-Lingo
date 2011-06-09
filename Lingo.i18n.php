2c5b6b3ab1aa80f423a6a7958bbab1868a4a3e5f
Raimond Spekking <raymond@users.mediawiki.org>
2011-06-09 20:58:25 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-06-09 20:44:00 UTC)

--- Lingo.i18n.php
+++ Lingo.i18n.php
@@ -26,7 +26,10 @@ $messages['qqq'] = array(
  * @author Kghbln
  */
 $messages['de'] = array(
-	'lingo-desc' => 'Ermöglicht Informationstexte die eingeblendet werden, sobald man mit der Maus über einen auf der Seite [[Terminology]] hinterlegten Begriff führt',
+	'lingo-desc' => 'Ermöglicht Informationstexte die eingeblendet werden, sobald man mit der Maus über einen auf der Seite [[$1]] hinterlegten Begriff führt',
+	'lingo-terminologypagename' => 'Glossar',
+	'lingo-noterminologypage' => 'Seite „$1“ ist nicht vorhanden.',
+	'lingo-terminologypagenotlocal' => 'Seite „$1“ befindet sich nicht auf diesem Wiki.',
 );
 
 /** French (Français)
@@ -36,11 +39,34 @@ $messages['fr'] = array(
 	'lingo-desc' => 'Dans les pages, affiche une info-bulle si le mot est défini dans la page de [[Terminology|Terminologie]].',
 );
 
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'lingo-desc' => 'Proporciona consellos nas páxinas ao pasar o rato por riba das palabras definidas na páxina "[[$1]]"',
+	'lingo-terminologypagename' => 'Terminoloxía',
+	'lingo-noterminologypage' => 'Non existe a páxina "$1".',
+	'lingo-terminologypagenotlocal' => 'A páxina "$1" non é unha páxina local.',
+);
+
 /** Macedonian (Македонски)
  * @author Bjankuloski06
  */
 $messages['mk'] = array(
 	'lingo-desc' => 'Овозможува описи при лебдење над алатките на страниците (од зборови со утврдени значења на страницата [[Terminology|Терминологија]])',
+	'lingo-terminologypagename' => 'Терминологија',
+	'lingo-noterminologypage' => 'Страницата „$1“ не постои.',
+	'lingo-terminologypagenotlocal' => 'Страницата „$1“ не е локална страница.',
+);
+
+/** Dutch (Nederlands)
+ * @author Tjcool007
+ */
+$messages['nl'] = array(
+	'lingo-desc' => "Zorgt voor tooltips op pagina's voor woorden gedefiniëerd op [[$1]].",
+	'lingo-terminologypagename' => 'Terminologie',
+	'lingo-noterminologypage' => "De pagina '$1' bestaat niet.",
+	'lingo-terminologypagenotlocal' => "De pagina '$1' is geen lokale pagina.",
 );
 
 /** Portuguese (Português)
@@ -48,6 +74,9 @@ $messages['mk'] = array(
  */
 $messages['pt'] = array(
 	'lingo-desc' => 'Permite que a passagem do ponteiro do rato sobre palavras nas páginas, resulte no aparecimento de uma legenda. As palavras são definidas na página [[Terminology]].',
+	'lingo-terminologypagename' => 'Terminology',
+	'lingo-noterminologypage' => "A página '$1' não existe.",
+	'lingo-terminologypagenotlocal' => "A página '$1' não é uma página local.",
 );
 
 /** Russian (Русский)
