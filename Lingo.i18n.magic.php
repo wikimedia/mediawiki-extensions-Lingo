ddec00c51ed3bf8a5a2243145eab2b38672369db
Siebrand Mazeland <siebrand@users.mediawiki.org>
2011-09-13 13:47:44 +0000

    Update for magic words for extensions from http://translatewiki.net.

--- Lingo.i18n.magic.php
+++ Lingo.i18n.magic.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Internationalisation file for Lingo extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
 
 $magicWords = array();
 
@@ -7,9 +13,27 @@ $magicWords['en'] = array(
 	'noglossary' => array( 0, '__NOGLOSSARY__' ),
 );
 
+/** Arabic (العربية) */
+$magicWords['ar'] = array(
+	'noglossary' => array( 0, '__لا_قاموس__' ),
+);
+
 /** German (Deutsch) */
-$magicWords['en'] = array(
+$magicWords['de'] = array(
 	'noglossary' => array( 0, '__KEIN_GLOSSAR__', '__NOGLOSSARY__' ),
 );
 
+/** Esperanto (Esperanto) */
+$magicWords['eo'] = array(
+	'noglossary' => array( 0, '__NEDIFINO__' ),
+);
 
+/** Nedersaksisch (Nedersaksisch) */
+$magicWords['nds-nl'] = array(
+	'noglossary' => array( 0, '__GIEN_GLOSSARIUM__' ),
+);
+
+/** Dutch (Nederlands) */
+$magicWords['nl'] = array(
+	'noglossary' => array( 0, '___GEENWOORDENLIJST__' ),
+);
