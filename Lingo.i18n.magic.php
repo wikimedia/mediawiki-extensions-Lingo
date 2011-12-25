ddbaa8a8d4833f085efcdca8d63888107126dad7
Siebrand Mazeland <siebrand@users.mediawiki.org>
2011-12-25 22:19:29 +0000

    Localisation updates for special pages and magic words for extensions from http://translatewiki.net.
    Backwards compatibility with MediaWiki 1.15 and earlier dropped for special page aliases.

--- Lingo.i18n.magic.php
+++ Lingo.i18n.magic.php
@@ -20,12 +20,17 @@ $magicWords['ar'] = array(
 
 /** German (Deutsch) */
 $magicWords['de'] = array(
-	'noglossary' => array( 0, '__KEIN_GLOSSAR__', '__NOGLOSSARY__' ),
+	'noglossary' => array( 0, '__KEINGLOSSAR__', '__KEIN_GLOSSAR__' ),
 );
 
 /** Esperanto (Esperanto) */
 $magicWords['eo'] = array(
 	'noglossary' => array( 0, '__NEDIFINO__' ),
+);
+
+/** Macedonian (Македонски) */
+$magicWords['mk'] = array(
+	'noglossary' => array( 0, '__БЕЗПОИМНИК__' ),
 );
 
 /** Nedersaksisch (Nedersaksisch) */
@@ -36,4 +41,9 @@ $magicWords['nds-nl'] = array(
 /** Dutch (Nederlands) */
 $magicWords['nl'] = array(
 	'noglossary' => array( 0, '___GEENWOORDENLIJST__' ),
+);
+
+/** Serbian (Latin script) (‪Srpski (latinica)‬) */
+$magicWords['sr-el'] = array(
+	'noglossary' => array( 0, '__BEZREČNIKA__', '__BEZ_REČNIKA__' ),
 );
