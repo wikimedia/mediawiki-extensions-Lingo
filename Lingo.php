339a718411d3c70a19050fe99ae7753ecbe6f468
Stephan Gambke <s7eph4n@gmail.com>
2014-03-21 23:34:16 +0000

    add branch alias for dev-master to composer.json
    Change-Id: I2d4ebfba7d53c18a34b16aee97bfbb7f2b3d1d8a

--- Lingo.php
+++ Lingo.php
@@ -8,7 +8,7 @@
  * @author    Barry Coughlan
  * @copyright 2010 Barry Coughlan
  * @author    Stephan Gambke
- * @version   1.1.0
+ * @version   1.1.1-dev
  * @licence   GNU General Public Licence 2.0 or later
  * @see       http://www.mediawiki.org/wiki/Extension:Lingo Documentation
  */
@@ -20,7 +20,7 @@ call_user_func( function () {
 		die( 'This file is part of a MediaWiki extension, it is not a valid entry point.' );
 	}
 
-	define( 'LINGO_VERSION', '1.1.0' );
+	define( 'LINGO_VERSION', '1.1.1-dev' );
 
 	// set defaults for settings
 
