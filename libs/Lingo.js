e7e11dc3b96a84431d7a4a2af0d6bf3f05951638
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2025-05-25 11:08:03 +0000

    build: Updating dependencies
    composer:
    * mediawiki/mediawiki-codesniffer: 46.0.0 → 47.0.0
    
    npm:
    * eslint-config-wikimedia: 0.28.2 → 0.30.0
      The following rules are failing and were disabled:
      * prefer-const
    
    Additional changes:
    * eslint: Replaced `wikimedia/client-es5` with `wikimedia/client`.
    
    Change-Id: I84a4283ba150a6623bc922888b84348b8afdd10c

--- libs/Lingo.js
+++ libs/Lingo.js
@@ -27,13 +27,13 @@
 ( function ( $ ) {
 	'use strict';
 
-	var lastFocus = null;
-	var isLastFocusInside = false;
+	let lastFocus = null;
+	let isLastFocusInside = false;
 
 	// eslint-disable-next-line no-shadow
-	$( function ( $ ) {
+	$( ( $ ) => {
 		$( 'a.mw-lingo-term' ).each( function () { // eslint-disable-line no-jquery/no-global-selector
-			var termId = $( this ).attr( 'data-lingo-term-id' ),
+			const termId = $( this ).attr( 'data-lingo-term-id' ),
 				tooltip = $( '#' + termId ); // eslint-disable-line no-jquery/variable-pattern
 
 			$( this ).qtip( {
@@ -58,7 +58,7 @@
 					lastFocus = $( this ); // eslint-disable-line no-jquery/variable-pattern
 					$( this ).qtip( 'show' );
 				} else if ( e.type === 'focusout' && isLastFocusInside ) {
-					var relatedTarget = $( e.relatedTarget ), // eslint-disable-line no-jquery/variable-pattern
+					const relatedTarget = $( e.relatedTarget ), // eslint-disable-line no-jquery/variable-pattern
 						api = lastFocus.qtip( 'api' ),
 						tooltipContent = api && api.elements.content;
 
@@ -71,20 +71,20 @@
 
 		} );
 
-		$( document ).on( 'keydown', function ( e ) {
+		$( document ).on( 'keydown', ( e ) => {
 			if ( e.key === 'Escape' && lastFocus ) {
 				lastFocus.qtip( 'hide' );
 				lastFocus.blur();
 				lastFocus.focus();
 				lastFocus = null;
 			} else if ( e.key === 'Tab' && lastFocus ) {
-				var api = lastFocus.qtip( 'api' ),
+				let api = lastFocus.qtip( 'api' ),
 					tooltipContent = api && api.elements.content,
 					tooltipDefinitions = tooltipContent && tooltipContent.find( '.mw-lingo-definition' ),
 					curFocus = $( ':focus' ); // eslint-disable-line no-jquery/variable-pattern, no-jquery/no-global-selector
 
 				if ( tooltipDefinitions.length > 0 ) {
-					var focusableElements = tooltipDefinitions.find( '.mw-lingo-definition-link > a' ),
+					const focusableElements = tooltipDefinitions.find( '.mw-lingo-definition-link > a' ),
 						currentIndex = focusableElements.index( curFocus );
 
 					if ( e.shiftKey ) {
